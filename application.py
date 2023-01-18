272a99b9f21995340a82f9e93351c5a8c98691ae
Stephen Crampton <stevesings@gmail.com>
2023-01-18 08:18:15 -0800

    upload and resize images

--- application.py
+++ application.py
@@ -1,42 +1,74 @@
-from flask import Flask
+import os
+
+from PIL import Image
+from flask import Flask, flash, request, redirect, send_from_directory, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = "./uploads"
+ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif"}
+
+app = Flask(__name__, static_url_path="/static")
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
-# print a nice greeting.
-def say_hello(username="World"):
-    return "<p>Hello %s!</p>\n" % username
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-# some bits of text for the page.
-header_text = """
-    <html>\n<head> <title>SteveC EB Flask Test</title> </head>\n<body>
-    <img src="/static/images/Stella.jpg">
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        # check if the post request has the file part
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            file.save(filepath)
+            basewidth = 300
+            img = Image.open(filepath)
+            wpercent = basewidth / float(img.size[0])
+            hsize = int((float(img.size[1]) * float(wpercent)))
+            img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+            dot_pos = filename.rfind(".")
+            filename_resized = filename[:dot_pos] + "-resized" + filename[dot_pos:]
+            filepath_resized = os.path.join(
+                app.config["UPLOAD_FOLDER"], filename_resized
+            )
+            img.save(filepath_resized)
+            return redirect(url_for("download_file", name=filename_resized))
+    return """
+    <!doctype html>
+    <title>Image Resizer</title>
+    <h1>Upload image file</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
     """
-instructions = """
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n"""
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = "</body>\n</html>"
 
-# EB looks for an 'application' callable by default.
-application = Flask(__name__, static_url_path="/static")
 
-# add a rule for the index page.
-application.add_url_rule(
-    "/", "index", (lambda: header_text + say_hello() + instructions + footer_text)
-)
+@app.route("/uploads/<name>")
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule(
-    "/<username>",
-    "hello",
-    (lambda username: header_text + say_hello(username) + home_link + footer_text),
-)
+
+app.add_url_rule("/uploads/<name>", endpoint="download_file", build_only=True)
+
+# EB looks for an "application" callable by default.
+application = app
 
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.debug = True
-    application.run()
+    app.debug = True
+    app.run()
